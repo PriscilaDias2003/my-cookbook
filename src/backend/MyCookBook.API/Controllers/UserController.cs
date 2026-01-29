a30a12e012142e6394f56f7c0511cf96fbd6a37d
PriscilaDias2003 <priscilagabrielle.ti@gmail.com>
2026-01-29 15:09:15 -0300

    feat: adding endpoint to register an user

---
+++ src/backend/MyCookBook.API/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyCookBook.Communication.Requests;
+using MyCookBook.Communication.Responses;
+
+namespace MyCookBook.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpPost]
+        [ProducesResponseType(typeof(ResponseRegisteredUserJson), StatusCodes.Status201Created)]
+        public IActionResult Register(RequestRegisterUserJson request) {
+            return Created();
+        }
+    }
+}
---
+++ src/shared/MyCookBook.Communication/Responses/ResponseRegisteredUserJson.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyCookBook.Communication.Responses
+{
+    public class ResponseRegisteredUserJson
+    {
+        public string Name { get; set; } = string.Empty;
+    }
+}
